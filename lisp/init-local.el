fa4cb7fab648e4584fc7f783b60ce4044adb7009
martin.haesler <martin.haesler@gmail.com>
2015-02-14 09:14:46 +0000

    load emmmet mode in web-mode

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -11,6 +11,7 @@
 (global-hl-line-mode t)
 (require-package 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-hook 'web-mode-hook 'emmet-mode t)
 
 (set-frame-font "Camingo Code 10")
 
